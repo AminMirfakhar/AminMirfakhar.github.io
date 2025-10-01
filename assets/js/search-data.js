766cb7d60444074787c0f7033b1c79b887e6d41f
AminMirfakhar <56076857+AminMirfakhar@users.noreply.github.com>
2025-10-01 16:23:45 +0000

    Deploying to gh-pages from @ AminMirfakhar/AminMirfakhar.github.io@7b86ddb3a7a1a76f723ca801d1964fb7e599788e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -106,13 +106,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/amin-mirfakhar-aa7398aa", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
